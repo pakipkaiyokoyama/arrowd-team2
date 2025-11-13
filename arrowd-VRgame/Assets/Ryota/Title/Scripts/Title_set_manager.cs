18c97eca0c5f9be713d788f384545625497659b9
Shuka <145205522+Shuka9061@users.noreply.github.com>
2025-11-13 22:13:07 +0900

    set VR controller

---
+++ arrowd-VRgame/Assets/Ryota/Title/Scripts/Title_set_manager.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title_setmanager : MonoBehaviour
+{
+    public void LoadMainScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
